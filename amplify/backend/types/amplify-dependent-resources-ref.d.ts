0decb0139d148d5b761e884ab6e900caed93d921
Alana La Bouchardiere <alana.labouchardiere@ausnetservices.com.au>
2022-12-05 08:31:02 +1100

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "plantkeeper": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, {useCallback, useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import {  StyleSheet, View, Text } from 'react-native';
+
+const Home = () => {
+
+
+
+
+  return (
+   <View>
+    <StatusBar/>
+    <Text>HEllooooo</Text>
+   </View>
+  );
+};
+
+export default Home;
